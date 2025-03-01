eb217da2ca85e790503a1e110233e5569400aa46
Santiago-Bonilla-21 <smbonilla@ucundinamarca.edu.co>
2025-03-01 17:11:33 -0500

    Se colocaron las explicaciones en el codigo main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 // Estiben Davila Niño
 // Calculator
 
-/// Otro forma sin interfaz grafica ///
+/// Otra forma sin interfaz grafica ///
 
 import java.util.Scanner ;
 
@@ -14,7 +14,7 @@ public class Main {
         boolean continuar1 = true ;
         while (continuar1) {
 
-
+            // Se colocan opciones para que el usuario elija que operación realizar
             System.out.println("Seleccion una operacion");
             System.out.println("1.SUMA ");
             System.out.println("2.RESTA");
@@ -39,32 +39,32 @@ public class Main {
                 System.out.print("Ingrese el número: ");
                 num1 = scanner.nextDouble();
             }
-
+            // Se hacen las operaciones de la calculadora
             double resultado = 0;
             switch (opcion) {
                 case 1:
-                    resultado = num1 + num2;
+                    resultado = num1 + num2; // Operación de suma
                     break;
                 case 2:
-                    resultado = num1 - num2;
+                    resultado = num1 - num2; // Operación de resta
                     break;
                 case 3:
-                    resultado = num1 * num2;
+                    resultado = num1 * num2; // Operación de multiplicación
                     break;
                 case 4:
-                    resultado = num1 / num2;
+                    resultado = num1 / num2; // operación de divición
                     break;
                 case 5:
-                    resultado = Math.pow(num1, num2);
+                    resultado = Math.pow(num1, num2); // operación de potenciación
                     break;
                 case 6:
-                    resultado = Math.sqrt(num1);
+                    resultado = Math.sqrt(num1); // operación de raiz
                     break;
                 case 7:
-                    resultado = num1 % num2;
+                    resultado = num1 % num2; // opreración de modulo
                     break;
                 case 8:
-                    resultado = (num1 * 9 / 5) + 32;
+                    resultado = (num1 * 9 / 5) + 32; //operación de converción
                     break;
                 default:
                     System.out.println("Opción no válida");
